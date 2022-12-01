5a58a735c1a7de2b11ce978e3c93e7f6d2c4d30a
sanseongko <tkstjd565@naver.com>
2022-12-02 01:27:07 +0900

    1,2,3 더하기(백준)
    백준 다이나믹 프로그래밍

---
+++ programmers/백준/다이나믹프로그래밍/123더하기.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+case = int(input())
+
+def dp(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+
+    return dp(n-1) + dp(n-2) + dp(n-3)
+
+for _ in range(case):
+    n = int(input())
+    print(dp(n))
+
+# n = 4 일 때부터 현재 숫자보다 3적은 수의 경우의 수부터 1작은 수의 경우의 수까지 더한 것이 현재수의 경우의 수가된다.
+# ex) 7의 경우의 수 -> 6의 경우의 수 + 5의 경우의 수 + 4의 경우의 수
+
+

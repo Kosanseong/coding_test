f5147500edd7e8fb6879ad1a3fd866451fc10c93
sanseongko <tkstjd565@naver.com>
2022-12-02 01:27:47 +0900

    input 값 추가

--- programmers/백준/다이나믹프로그래밍/123더하기.py
+++ programmers/백준/다이나믹프로그래밍/123더하기.py
@@ -1,5 +1,5 @@
 import sys
-
+#input.txt -> 3,4,7,10
 sys.stdin = open("input.txt", "r")
 case = int(input())
 
@@ -19,5 +19,4 @@ for _ in range(case):
 
 # n = 4 일 때부터 현재 숫자보다 3적은 수의 경우의 수부터 1작은 수의 경우의 수까지 더한 것이 현재수의 경우의 수가된다.
 # ex) 7의 경우의 수 -> 6의 경우의 수 + 5의 경우의 수 + 4의 경우의 수
-
 

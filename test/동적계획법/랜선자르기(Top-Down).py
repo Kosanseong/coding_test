fb9ba6e9145fe6f8a90cc67ee6b5ff52b745b514
sanseongko <tkstjd565@naver.com>
2022-12-03 07:25:58 +0900

    랜선 자르기 문제 메모이제이션 추가

--- test/동적계획법/랜선자르기(Top-Down).py
+++ test/동적계획법/랜선자르기(Top-Down).py
@@ -3,14 +3,15 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 n = int(input())
-# 각 스텝마다 들어갈 경우의 수
+# 각 스텝마다 들어갈 경우의 수(메모이제이션)
 dy = [0] * (n+1)
+dy[1] = 1
+dy[2] = 2
 
 def DFS(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
+    # dy 배열에 값이 있다면 굳이 연산할 필요없이 리턴해버린다
+    if dy[n] != 0:
+        return dy[n]
 
     return DFS(n-1) + DFS(n-2)
 
